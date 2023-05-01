2d17b6da1f6a08a63b0a0f302bd7c24e8f60fbaf
waley-code <walephlp@gmail.com>
2023-05-01 14:55:59 +0000

    completed task 5&6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,17 +3,24 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy import text
 
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
     counts = {
-            'amenities': storage.count('Amenity'),
-            'cities': storage.count('City'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('User')
+            'amenities': storage.count(text('Amenity')),
+            'cities': storage.count(text('City')),
+            'places': storage.count(text('Place')),
+            'reviews': storage.count(text('Review')),
+            'states': storage.count(text('State')),
+            'users': storage.count(text('User'))
                                                                     }
     return jsonify(counts)
 
